59ae645ef4ed1d14478a76f910716a8b2d4a1b4f
Jon Bebi <dr.jon.bebi@gmail.com>
2019-07-25 19:57:14 -0400

    updated

---
+++ week-2/config1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 15:02:44 2019
+
+@author: flatironschool
+"""
+
+host = "yelppp.cdhanbzj0dic.us-east-2.rds.amazonaws.com"
+user = "omarqusous"
+password = "jonandomar07"
