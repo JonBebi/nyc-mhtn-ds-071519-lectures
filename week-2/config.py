7e77e97a4437fbfed785a2e7c247b8bf79ca207f
Jon Bebi <dr.jon.bebi@gmail.com>
2019-07-28 19:42:44 -0400

    updated

--- week-2/config.py
+++ week-2/config.py
@@ -6,6 +6,6 @@ Created on Thu Jul 25 15:02:44 2019
 @author: flatironschool
 """
 
-host = "yelppp.cdhanbzj0dic.us-east-2.rds.amazonaws.com"
+host = "flatiron.cdhanbzj0dic.us-east-2.rds.amazonaws.com"
 user = "omarqusous"
 password = "jonandomar07"
